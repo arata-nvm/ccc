5eb34f6da107faf7998435e17f8da6e03d7841a1
arata <arata04962@gmail.com>
2020-04-10 23:59:17 +0900

    Split main.c into multiple small files

---
+++ chibi.h
@@ -0,0 +1,62 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// tokenize.c
+
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
+  int len;
+};
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+bool consume(char *op);
+void expect(char *op);
+long expect_number(void);
+bool at_eof(void);
+Token *tokenize(void);
+
+extern char *user_input;
+extern Token *token;
+
+// parse.c
+
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_EQ,
+  ND_NE,
+  ND_LT,
+  ND_LE,
+  ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  long val;
+};
+
+Node *expr(void);
+
+// codegen.c
+
+void codegen(Node *node);
