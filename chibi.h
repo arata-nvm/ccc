c391ff331d132ddfa94936ed71253aa8269d94dd
arata <arata04962@gmail.com>
2020-04-12 11:25:53 +0900

    Support function definition up to 6 parameters

--- chibi.h
+++ chibi.h
@@ -41,9 +41,14 @@ extern Token *token;
 
 typedef struct Var Var;
 struct Var {
-  Var *next;
   char *name;
   int offset;
+};
+
+typedef struct VarList VarList;
+struct VarList {
+  VarList *next;
+  Var *var;
 };
 
 typedef enum {
@@ -94,8 +99,10 @@ typedef struct Function Function;
 struct Function {
   Function *next;
   char *name;
+  VarList *params;
+
   Node *node;
-  Var *locals;
+  VarList *locals;
   int stack_size;
 };
 
--- parse.c
+++ parse.c
@@ -1,12 +1,14 @@
 #include "chibi.h"
 
-Var *locals;
+static VarList *locals;
 
 static Var *find_var(Token *tok) {
-  for (Var *var = locals; var; var = var->next)
+  for (VarList *vl = locals; vl; vl = vl->next) {
+    Var *var = vl->var;
     if (strlen(var->name) == tok->len &&
         !strncmp(tok->str, var->name, tok->len))
       return var;
+  }
   return NULL;
 }
 
@@ -43,9 +45,11 @@ static Node *new_var_node(Var *var) {
 
 static Var *new_lvar(char *name) {
   Var *var = calloc(1, sizeof(Var));
-  var->next = locals;
   var->name = name;
-  locals = var;
+  VarList *vl = calloc(1, sizeof(VarList));
+  vl->var = var;
+  vl->next = locals;
+  locals = vl;
   return var;
 }
 
@@ -72,24 +76,40 @@ Function *program(void) {
   return head.next;
 }
 
+static VarList *read_func_params(void) {
+  if (consume(")"))
+    return NULL;
+
+  VarList *head = calloc(1, sizeof(VarList));
+  head->var = new_lvar(expect_ident());
+  VarList *cur = head;
+
+  while (!consume(")")) {
+    expect(",");
+    cur->next = calloc(1, sizeof(VarList));
+    cur->next->var = new_lvar(expect_ident());
+    cur = cur->next;
+  }
+
+  return head;
+}
+
 static Function *function(void) {
   locals = NULL;
 
-  char *name = expect_ident();
+  Function *fn = calloc(1, sizeof(Function));
+  fn->name = expect_ident();
   expect("(");
-  expect(")");
+  fn->params = read_func_params();
   expect("{");
 
   Node head = {};
   Node *cur = &head;
-
   while (!consume("}")) {
     cur->next = stmt();
     cur = cur->next;
   }
 
-  Function *fn = calloc(1, sizeof(Function));
-  fn->name = name;
   fn->node = head.next;
   fn->locals = locals;
   return fn;
